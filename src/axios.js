d0d977c07b28cf131e578132d610b1f4c483d460
varunjeett <abhijeetvarun@gmail.com>
2020-10-11 00:38:04 +0530

    ch

--- src/axios.js
+++ src/axios.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://us-central1-buncholunch-89ce8.cloudfunctions.net/api"
-  // baseURL: "http://localhost:5001/buncholunch-89ce8/us-central1/api", // the cloud function url
+  
+  baseURL: "http://localhost:5001/buncholunch-89ce8/us-central1/api"
+  
 });
 
 export default instance;
